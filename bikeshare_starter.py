9b67c7d392ae452f487a29fd2879c51557d11508
Lingjia Gardner <lingjia.gardner@cariad.technology>
2026-02-18 20:50:32 +0100

    Add LA as city in bikeshare_starter.py

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -4,7 +4,7 @@ import numpy as np
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv', 'boston': 'boston.csv' }
+              'washington': 'washington.csv', 'boston': 'boston.csv', 'LA': 'LA.csv' }
 
 def get_filters():
     """
